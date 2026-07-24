b0a227d1dbd0a3edb2cd341d2e76819155672634
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2026-07-24 09:21:11 +0700

    fix: update bin

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-import { run } from '@stricli/core'
-import { app } from '@/app'
-import { buildContext } from '@/context'
+import { run } from '@stricli/core';
+import { app } from '@/app';
+import { buildContext } from '@/context';
 
-await run(app, process.argv.slice(2), buildContext(process))
+(async () => {
+  run(app, process.argv.slice(2), buildContext(process));
+})();
